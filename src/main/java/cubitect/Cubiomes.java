c12a026388da734453205caea0b06f25936e81f8
Antonio Cheong <teapotv8@proton.me>
2024-01-13 15:20:20 +0000

    embed features as enum

--- src/main/java/cubitect/Cubiomes.java
+++ src/main/java/cubitect/Cubiomes.java
@@ -11,16 +11,44 @@ public class Cubiomes {
 	}
 
 	public static native int main();
-	
+
 	private static native long getNearestStructure(int structType, int x, int z, long seed, int mc_version, int searchSize);
 	
 	public static class Pos {
 		public int x;
 		public int z;
+		public int y;
 	}
 
-	public static Pos GetNearestStructure(int structType, int x, int z, Long seed, int mc_version, int searchSize) {
-		long result = getNearestStructure(structType, x, z, seed, mc_version, searchSize);
+	public static enum StructureType {
+		Feature, // for locations of temple generation attempts pre 1.13
+    Desert_Pyramid,
+    Jungle_Temple,
+    Swamp_Hut,
+    Igloo,
+    Village,
+    Ocean_Ruin,
+    Shipwreck,
+    Monument,
+    Mansion,
+    Outpost,
+    Ruined_Portal,
+    Ruined_Portal_N,
+    Ancient_City,
+    Treasure,
+    Mineshaft,
+    Desert_Well,
+    Geode,
+    Fortress,
+    Bastion,
+    End_City,
+    End_Gateway,
+    Trail_Ruin,
+    FEATURE_NUM
+	}
+
+	public static Pos GetNearestStructure(StructureType structType, int x, int z, Long seed, int mc_version, int searchSize) {
+		long result = getNearestStructure(structType.ordinal(), x, z, seed, mc_version, searchSize);
 		if (result == 0) {
 			return null;
 		}
