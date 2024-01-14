5b464781bf4445c8764be8eb20a76efa2a4ec9ef
Antonio Cheong <teapotv8@proton.me>
2024-01-14 20:18:50 +0000

    test package

---
+++ test/src/main/java/com/testing/MyApplication.java
@@ -0,0 +1,17 @@
+package com.testing;
+import java.math.BigInteger;
+
+import cubitect.Cubiomes;
+import cubitect.Cubiomes.Pos;
+// src/MyApplication.java
+public class MyApplication {
+    public static void main(String[] args) {
+        // Example usage of a method from the HawtJNI library
+        // Test myJniLibrary = new Test();
+        Cubiomes.main();
+				long seed = new BigInteger("-1717312650922586601").longValue();
+				Pos pos = Cubiomes.GetNearestStructure(Cubiomes.StructureType.Stronghold, 0, 0, seed, Cubiomes.MCVersion.MC_1_20, 8);
+				System.out.println(pos.x + " " + pos.z);
+    }
+}
+
