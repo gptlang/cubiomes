15aceaa8bdf27935f194db29f71a5415536a59a3
Antonio Cheong <teapotv8@proton.me>
2024-01-15 13:18:14 +0000

    fix stupidity that caused end and nether to not work

--- src/main/native-package/src/main.c
+++ src/main/native-package/src/main.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 
 int main() {
-  int structType = Village;
+  int structType = Bastion;
   int mc = MC_1_20;
 
   int x = 0;
@@ -19,11 +19,6 @@ int main() {
   x = (int)(p >> 32);
   z = (int)(p & 0xffffffffL);
   printf("Coordinates of nearest structure: %d, %d\n", x, z);
-	long long stronghold = getNearestStronghold(x, z, seed, mc, 8);
-	x = (int)(stronghold >> 32);
-	z = (int)(stronghold & 0xffffffffL);
-	printf("Coordinates of nearest stronghold: %d, %d\n", x, z);
-	return 0;
 }
 
 
@@ -95,7 +90,7 @@ long long getNearestStructure(int structType, int x, int z, uint64_t seed,
 				return -1;
 			}
 	}
-  applySeed(&g, DIM_OVERWORLD, seed);
+  applySeed(&g, dimension, seed);
 
   // Regions are 32x32 chunks
   int startX = x / 16 / 32; // convert coordinates to chunk coordinates
