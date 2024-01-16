f51c4c7f8bfc697588337f669e83bc6249b4df37
Antonio Cheong <teapotv8@proton.me>
2024-01-16 03:54:19 +0000

    use submodules and pray nothing breaks

--- src/main/native-package/src/main.h
+++
@@ -1,5 +0,0 @@
-#include "finders.h"
-int main();
-long long getNearestStructure(int structType, int x, int z, uint64_t seed, int mc);
-long long getNearestStronghold(int x, int z, uint64_t seed, int mc);
-int getDimFromFeature(enum StructureType structType);
