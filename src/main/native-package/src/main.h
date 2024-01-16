16f6f6c0051bb599d767a74fea03096906f5e1d7
Antonio Cheong <teapotv8@proton.me>
2024-01-16 03:00:40 +0000

    remove unused function def

--- src/main/native-package/src/main.h
+++ src/main/native-package/src/main.h
@@ -3,4 +3,3 @@ int main();
 long long getNearestStructure(int structType, int x, int z, uint64_t seed, int mc);
 long long getNearestStronghold(int x, int z, uint64_t seed, int mc);
 int getDimFromFeature(enum StructureType structType);
-int max(int a, int b);
